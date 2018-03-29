f0b0cac3626edeb5a810ed097fadc2ad5cb0c2bd
Fabien SCOLAN <fabien.scolan22@gmail.com>
2018-03-29 11:01:00 +0200

    Exercices Functions complété

--- src/test/java/java8/ex05/Function_05_Test.java
+++ src/test/java/java8/ex05/Function_05_Test.java
@@ -12,34 +12,28 @@ import java.util.function.Consumer;
  */
 public class Function_05_Test {
 
-    //tag::functions[]
-    // TODO compléter la fonction
-    // TODO modifier le mot de passe en "secret"
-    Consumer<Person> changePasswordToSecret = (p) -> p.setPassword("secret");
+	// tag::functions[]
 
-    // TODO compléter la fonction
-    // TODO vérifier que l'age > 4 avec une assertion JUnit
-    Consumer<Person> verifyAge = (p) -> {assert p.getAge()>4;};
+	Consumer<Person> changePasswordToSecret = (p) -> p.setPassword("secret");
 
-    // TODO compléter la fonction
-    // TODO vérifier que le mot de passe est "secret" avec une assertion JUnit
-    Consumer<Person> verifyPassword = (p) -> {	assert p.getPassword().equals("secret");} ;
-    //end::functions[]
+	Consumer<Person> verifyAge = (p) -> {
+		assert p.getAge() > 4;
+	};
 
+	Consumer<Person> verifyPassword = (p) -> {
+		assert p.getPassword().equals("secret");
+	};
+	// end::functions[]
 
-    @Test
-    public void test_consumer() throws Exception {
-        List<Person> personList = Data.buildPersonList();
+	@Test
+	public void test_consumer() throws Exception {
+		List<Person> personList = Data.buildPersonList();
 
-        // TODO invoquer la méthode personList.forEach pour modifier les mots de passe en "secret"
-        personList.forEach(changePasswordToSecret);
+		personList.forEach(changePasswordToSecret);
 
-        // TODO remplacer la boucle for par l'invocation de la méthode forEach
-        // TODO Utiliser la méthode andThen pour chaîner les vérifications verifyAge et verifyPassword
-        // personList.forEach...
-        personList.forEach(verifyAge);
-        personList.forEach(verifyPassword);
+		// personList.forEach...
+		personList.forEach(verifyAge);
+		personList.forEach(verifyPassword);
 
-
-    }
+	}
 }
--- src/test/java/java8/ex06/Function_06_Test.java
+++ src/test/java/java8/ex06/Function_06_Test.java
@@ -1,10 +1,10 @@
 package java8.ex06;
 
-
 import java8.data.Person;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.omg.Messaging.SyncScopeHelper;
 
 import java.util.Objects;
 import java.util.function.Supplier;
@@ -14,39 +14,39 @@ import java.util.function.Supplier;
  */
 public class Function_06_Test {
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
 
-    // tag::formatAge[]
-    // TODO compléter la méthode
-    // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
-    String formatAge(Supplier<Person> supplier) {
-        // TODO
-        return null;
-    }
-    // end::formatAge[]
+	// tag::formatAge[]
+	// (exemple : [age=12])
+	String formatAge(Supplier<Person> supplier) {
+		// TODO
+		return String.format("[age=%d]", supplier.get().getAge());
+	}
+	// end::formatAge[]
 
-
-    @Test
+	@Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+		Supplier<Person> sup = () -> {return new Person("A", "B", 35, "test");};
+		String result = formatAge(sup);
 
         assert result.equals("[age=35]");
     }
 
-    @Test
-    public void test_supplier_requireNonNull() throws Exception {
+	@Test
+	public void test_supplier_requireNonNull() throws Exception {
 
-        expectedException.expect(NullPointerException.class);
-        expectedException.expectMessage("require non null object");
+		expectedException.expect(NullPointerException.class);
+		expectedException.expectMessage("require non null object");
 
-        // TODO compléter le test unitaire pour qu'il soit passant
-        Supplier<String> supplier = null;
+		// TODO compléter le test unitaire pour qu'il soit passant
+		Supplier<String> supplier = () -> {return "require non null object"; };
 
-        // Avec un paramètre null, cette méthode déclenche un NullPointerException
-        Objects.requireNonNull(null, supplier);
+		// Avec un paramètre null, cette méthode déclenche un
+		// NullPointerException
+		Objects.requireNonNull(null, supplier);
 
-    }
+	}
 
 }
